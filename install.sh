5a2527c70ed9722e6d84ac77b69431045cc282c4
Mike Priscella <mpriscella@gmail.com>
2020-10-07 23:13:09 -0400

    Pass -y flag into drush install commands

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ drush -y config-set system.performance css.preprocess 0
 drush -y config-set system.performance js.preprocess 0
 
 # Install extensions.
-yq read config.yml "modules.enable[*]" | while read p; do drush pm:enable "$p"; done
-yq read config.yml "themes.enable[*]" | while read p; do drush theme:enable "$p"; done
+yq read config.yml "modules.enable[*]" | while read p; do drush -y pm:enable "$p"; done
+yq read config.yml "themes.enable[*]" | while read p; do drush -y theme:enable "$p"; done
 
 # Rebuild cache.
 drush cr
