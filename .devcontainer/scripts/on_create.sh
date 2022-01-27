5fe1d13b0e735b8e6b0059850050c1e7dc2e46eb
Mike Priscella <mpriscella@gmail.com>
2022-01-27 05:43:47 +0000

    Run apache start on postcreate

--- .devcontainer/scripts/on_create.sh
+++ .devcontainer/scripts/on_create.sh
@@ -4,22 +4,12 @@ sudo rm -rf /var/www/html
 
 sudo git clone https://git.drupalcode.org/project/drupal.git /var/www/html
 
-sudo chown -R vscode:vscode /var/www/html
-
-# cd /var/www/html
+sudo chown -R vscode:root /var/www/html
 
 echo "vendor\nmodules\nprofiles\nthemes" > /var/www/html/.gitignore
 
-cd /home/vscode
-
-composer require drush/drush
-
 cd /var/www/html
 
-# composer install
+composer install
 
-# composer install
-
-# drush install
-
-./bin/drush site-install --db-url=mysql://drupal:foundation@127.0.0.1:3306/drupal --existing-config --account-pass=admin
+composer require drush/drush
