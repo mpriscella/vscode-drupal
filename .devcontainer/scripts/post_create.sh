6da0aa7331069bc8751a0b5a92954866d587deaf
Mike Priscella <mpriscella@gmail.com>
2022-02-02 04:21:01 +0000

    Post Create should setup drupal.org SSH Key

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -2,8 +2,6 @@
 
 sudo rm -rf /var/www/html && sudo ln -s $CODESPACE_VSCODE_FOLDER/drupal /var/www/html
 
-# apache2ctl start
-
 mkdir -p /vscode/.ssh
-echo -e "$DRUPAL_SSH_PRIVATE_KEY\n" > /vscode/.ssh/id_rsa
-chmod 400 /vscode/.ssh/id_rsa
+echo -e "$DRUPAL_SSH_PRIVATE_KEY\n" > /home/vscode/.ssh/id_rsa
+chmod 400 /home/vscode/.ssh/id_rsa
