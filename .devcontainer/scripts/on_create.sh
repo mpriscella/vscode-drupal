87fbb5baa7a80a859a161eb91137e69836c51672
Mike Priscella <mpriscella@gmail.com>
2022-02-09 16:40:26 +0000

    Add onCreate and postCreate to devcontainer

--- .devcontainer/scripts/on_create.sh
+++ .devcontainer/scripts/on_create.sh
@@ -4,9 +4,6 @@ if [ ! -f drupal/composer.json ]; then
   git submodule update --init
 fi
 
-sudo rm -rf /var/www/html
-sudo ln -s $CODESPACE_VSCODE_FOLDER/drupal /var/www/html
-
 cat > drupal/.gitignore <<- EOM
 vendor
 modules
@@ -15,8 +12,6 @@ themes
 sites/default/files
 EOM
 
-cd $CODESPACE_VSCODE_FOLDER/drupal
-
-composer install
-
-composer require drush/drush
+cd $CODESPACE_VSCODE_FOLDER/drupal && \
+  composer install && \
+  composer require drush/drush
--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo rm -rf /var/www/html && sudo ln -s $CODESPACE_VSCODE_FOLDER/drupal /var/www/html
-
-mkdir -p /vscode/.ssh
-echo -e "$DRUPAL_SSH_PRIVATE_KEY\n" > /home/vscode/.ssh/id_rsa
-chmod 400 /home/vscode/.ssh/id_rsa
+if [ ! -f /home/vscode.ssh/id_rsa ];then
+  mkdir -p /home/vscode/.ssh
+  echo -e "$DRUPAL_SSH_PRIVATE_KEY\n" > /home/vscode/.ssh/id_rsa
+  chmod 400 /home/vscode/.ssh/id_rsa
+fi
